cc63828728efd7c4c11a41af73d3755607809b8b
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2025-02-21 15:50:26 +0530

    feat: login page added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,17 @@
-import { LoginForm } from '@/components/login-form';
-import { GalleryVerticalEnd } from 'lucide-react';
-import AILoginPage from '../assets/aiLoginpage.jpg';
+import { GalleryVerticalEnd } from "lucide-react"
 
-export default function Login() {
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex h-6 w-6 items-center justify-center rounded-md">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEnd className="size-4" />
             </div>
-            edu.AI
+            Acme Inc.
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
@@ -20,13 +20,13 @@ export default function Login() {
           </div>
         </div>
       </div>
-      <div className="bg-muted relative hidden lg:block">
+      <div className="relative hidden bg-muted lg:block">
         <img
-          src={AILoginPage}
+          src="/placeholder.svg"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
     </div>
-  );
+  )
 }
