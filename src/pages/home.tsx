79315bf734e44433644479ba46a1bef53ac3e9a9
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2025-02-21 15:39:28 +0530

    feat: home page added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,31 @@
+import Navbar from '@/components/navbar';
+import { Button } from '@/components/ui/button';
+import { useNavigate } from 'react-router-dom';
+
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <>
-      <div className="flex min-h-screen w-full flex-col items-center justify-center gap-y-3">
-        <h1 className="text-3xl font-bold underline">Hello Home...!</h1>
-      </div>
+      <main className="bg-home min-h-[90vh] w-full">
+        <Navbar />
+
+        <section className="container mx-auto flex h-full min-h-[95vh] w-full flex-col items-center justify-center gap-y-5">
+          <h2 className="text-center text-5xl font-bold leading-tight text-white lg:w-[50%]">
+            Welcome to edu<b className="text-green-600">.</b>
+            <i className="text-orange-600">AI</i> Your Gateway to Smarter
+            Learning
+          </h2>
+
+          <p className="text-center text-xl font-semibold text-gray-300 lg:w-[40%]">
+            Learn at your own pace with engaging courses, quizzes, and progress
+            tracking. Start your journey today!
+          </p>
+
+          <Button onClick={() => navigate('/dashboard')} variant={'secondary'}>
+            Start Learning
+          </Button>
+        </section>
+      </main>
     </>
   );
 }
